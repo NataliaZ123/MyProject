e3f054e20b6e90968d8cb73bdb9ba998111fb46d
NataliaZ123 <123448567+NataliaZ123@users.noreply.github.com>
2023-01-25 22:43:05 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -18,7 +18,6 @@ public class Main{
             a = Integer.parseInt(String.valueOf(input.charAt(0)));
 			b = Integer.parseInt(String.valueOf(input.charAt(4)));
 			System.out.println(input);
-			if((a > 10) || (b > 10)){
 				return("throws Exception");
 			}
 			switch (input.charAt(2)){
